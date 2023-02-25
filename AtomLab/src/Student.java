201b441552e69f333288d5d1de0a60b09518b971
Len0k <114250252+LenaGosteva@users.noreply.github.com>
2023-02-25 10:49:05 +0300

    Delete AtomLab directory

--- AtomLab/src/Student.java
+++
@@ -1,5 +0,0 @@
-public class Student extends Person {
-    public Student(int id, String name, String surname, int age, String login, String password) {
-        super(id, name, surname, age, login, password);
-    }
-}
